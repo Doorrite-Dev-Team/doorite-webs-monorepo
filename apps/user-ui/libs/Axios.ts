8f55c7154e80e86fee5e0fa7d5b54203ca9d74b3
Abu AbdirRahman <abdulazeezadekiilekun@gmail.com>
2025-08-17 11:47:24 +0100

    Fix: Build error

--- apps/user-ui/libs/Axios.ts
+++ apps/user-ui/libs/Axios.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
+
+// This will show in your production logs
+// console.log("Production NEXT_PUBLIC_API_URI:", process.env.NEXT_PUBLIC_API_URI);
+// console.log("NODE_ENV:", process.env.NODE_ENV);
+
 const Axios = axios.create({
-  baseURL: process.env.API_URI
-    ? `${process.env.API_URI}/api/v1`
-    : "http://localhost:4000/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URI
+    ? `${process.env.NEXT_PUBLIC_API_URI}/api/v1`
+    : "http://localhost:4000/api/v1", // This fallback won't work in production
   withCredentials: true,
   timeout: 5 * 60 * 1000,
 });
