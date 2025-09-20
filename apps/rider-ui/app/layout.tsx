ea3ccfdb49841aae1a817af2f67d04a9ad65b912
abuAbdur-rahman <abdulazeezadekiilekun@gmail.com>
2025-09-20 08:22:44 +0100

    create: Doorrite Vendor UI

---
+++ apps/rider-ui/app/layout.tsx
@@ -0,0 +1,38 @@
+import "@repo/ui/globals.css";
+import type { Metadata } from "next";
+import { Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+const plus_Jakarta_Sans = Plus_Jakarta_Sans({
+  variable: "--font-plus-jakarta-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Doorite Vendor UI",
+  description: "Vendor interface for Doorite",
+  icons: {
+    icon: "/icon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${plus_Jakarta_Sans.variable} ${geistMono.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ apps/rider-ui/app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@repo/ui/components/button";
+
+const Home = () => {
+  return (
+    <div>
+      Home <Button>Hello World</Button>
+    </div>
+  );
+};
+
+export default Home;
