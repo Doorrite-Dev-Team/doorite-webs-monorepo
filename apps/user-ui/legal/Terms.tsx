682d53b8a600eede235d5928f791d7142308f8cd
Abu Abdir-rahman <abdulazeezadekiilekun@gmail.com>
2025-12-14 08:22:28 +0100

    fix errors
    Signed-off-by: Abu Abdir-rahman <abdulazeezadekiilekun@gmail.com>

---
+++ apps/user-ui/legal/Terms.tsx
@@ -0,0 +1,12 @@
+//legal/terms.tsx
+"use client";
+
+import { MdxClientWrapper } from "@/components/MdxClientWrapper";
+import TermsContent from "@/legal/term.mdx";
+// import * as React from 'react';
+
+const Terms = () => {
+  return <MdxClientWrapper content={TermsContent} />;
+};
+
+export default Terms;
